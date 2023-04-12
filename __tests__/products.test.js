220a35610e15370698f710992e9bce2bd9bcd278
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-04-12 12:32:17 +0200

    Adds products' tests

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -20,13 +20,23 @@ const validProduct = {
   price: 10000,
 }
 
+const notValidProduct = {
+  description: "Good phone",
+  price: 10000,
+}
+
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_TEST_URL)
   const product = new ProductsModel(validProduct)
   await product.save()
 }) // beforeAll is a Jest hook which will be ran before all tests, usually this is used to connect to db and to do some initial setup like adding some mock data to the db
 
-describe("Test APIs", () => {
+afterAll(async () => {
+  await ProductsModel.deleteMany()
+  await mongoose.connection.close()
+}) // afterAll hook could to clean up the situation (close the connection to Mongo gently and clean up db/collections)
+
+describe("Test Products APIs", () => {
   // it("Should test that GET /test endpoint returns 200 and a body containing a message", async () => {
   //   const response = await client.get("/test")
   //   expect(response.status).toBe(200)
@@ -34,5 +44,19 @@ describe("Test APIs", () => {
   // })
   it("Should test that env vars are loaded correctly", () => {
     expect(process.env.MONGO_TEST_URL).toBeDefined()
+  })
+
+  it("Should test that POST /products returns 201 and an _id if a valid product is provided in req.body", async () => {
+    const response = await client.post("/products").send(validProduct).expect(201)
+    expect(response.body._id).toBeDefined()
+  })
+
+  it("Should test that POST /products returns 400 if a not valid product is provided in req.body", async () => {
+    await client.post("/products").send(notValidProduct).expect(400)
+  })
+
+  it("Should test that GET /products returns 200 and a body", async () => {
+    const response = await client.get("/products").expect(200)
+    console.log(response.body)
   })
 })
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import Express from "express"
 import cors from "cors"
+import productsRouter from "./api/products/index.js"
+import { badRequestHandler, genericErrorHandler } from "./errorHandlers.js"
 
 const server = Express()
 
@@ -8,10 +10,14 @@ server.use(cors())
 server.use(Express.json())
 
 // ************************************** ENDPOINTS ***********************************
+server.use("/products", productsRouter)
+
 // server.get("/test", (req, res, next) => {
 //   res.status(201).send({ message: "TEST SUCCESSFULL" })
 // })
 
 // ************************************* ERROR HANDLERS *******************************
+server.use(badRequestHandler)
+server.use(genericErrorHandler)
 
 export default server
